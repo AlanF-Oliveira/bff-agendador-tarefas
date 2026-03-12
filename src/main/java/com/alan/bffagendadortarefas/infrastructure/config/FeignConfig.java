26fdf0027d76f473ac428a19b8fb1dd8e025cb4a
Alan Ferreira <131145088+AlanF-Oliveira@users.noreply.github.com>
2026-03-12 19:13:44 -0300

    fix: adiciona configuração do OkHttpClient para suporte ao método PATCH no Feign

---
+++ src/main/java/com/alan/bffagendadortarefas/infrastructure/config/FeignConfig.java
@@ -0,0 +1,15 @@
+package com.alan.bffagendadortarefas.infrastructure.config;
+
+import feign.Client;
+import okhttp3.OkHttpClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    public Client feignClient() {
+        return new feign.okhttp.OkHttpClient(new OkHttpClient());
+    }
+}
